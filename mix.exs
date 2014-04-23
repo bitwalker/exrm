91988e5d3a58a806b8e3831ae0e05148c71da12f
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-04-22 22:43:28 -0500

    Add hex.pm metadata

--- mix.exs
+++ mix.exs
@@ -1,24 +1,30 @@
+Code.ensure_loaded?(Hex) and Hex.start
+
 defmodule ReleaseManager.Mixfile do
   use Mix.Project
 
   def project do
     [ app: :exrm,
       version: "0.4.6",
-      deps: deps ]
+      description: description,
+      package: package,
+      deps: [] ]
   end
 
-  # Configuration for the OTP application
-  def application do
-    []
+  def application, do: []
+
+  defp description do
+    """
+    Exrm, or Elixir Release Manager, provides mix tasks for building, 
+    upgrading, and controlling release packages for your application.
+    """
   end
 
-
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
-  defp deps do
-    []
+  defp package do
+    [ files: ["lib", "priv", "mix.exs", "README.md", "LICENSE"],
+      contributors: ["Paul Schoenfelder"],
+      licenses: ["MIT"],
+      links: [ { "GitHub", "https://github.com/bitwalker/exrm" } ] ]
   end
+
 end
