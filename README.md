b20c9407e244fbca8c1986848b1063fcfd2c01d3
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-05-19 23:18:42 -0500

    Version 0.6.7

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ This project's goal is to make releases with Elixir projects a breeze. It is com
 
 ```elixir
   defp deps do
-    [{:exrm, "~> 0.6.5"}]
+    [{:exrm, "~> 0.6.7"}]
   end
 ```
 
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ReleaseManager.Mixfile do
 
   def project do
     [ app: :exrm,
-      version: "0.6.6",
+      version: "0.6.7",
       elixir: "~> 0.13.2-dev",
       description: description,
       package: package,
