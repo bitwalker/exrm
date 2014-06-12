79e667606c63e79440faeb60cc6ab686e8bd4cc7
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-06-12 11:16:23 -0500

    Support 0.14.1-dev

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule ReleaseManager.Mixfile do
 
   def project do
     [ app: :exrm,
-      version: "0.9.0",
-      elixir: "~> 0.14.0-dev",
+      version: "0.10.0",
+      elixir: "~> 0.14.1-dev",
       description: description,
       package: package,
       deps: [{:conform, github: "bitwalker/conform", branch: "develop"}] ]
