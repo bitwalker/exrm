5bf7cb6acb973e383858f32b7adc1e30095dfb02
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-07-20 15:13:40 -0500

    Update conform to 0.8.5

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ReleaseManager.Mixfile do
       elixir: "~> 0.14.3",
       description: description,
       package: package,
-      deps: [{:conform, "~> 0.8.4"}] ]
+      deps: [{:conform, "~> 0.8.5"}] ]
   end
 
   def application, do: []
