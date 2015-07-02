22eafa53983cdcd9d02e7e67296c082e38754108
Akash Manohar J <akash@akash.im>
2015-07-02 09:49:08 +0530

    Remove unused get_real_path/1

--- lib/exrm/utils.ex
+++ lib/exrm/utils.ex
@@ -309,20 +309,4 @@ defmodule ReleaseManager.Utils do
     end
   end
 
-  defp get_real_path(path) do
-    case path |> String.to_char_list |> :file.read_link_info do
-      {:ok, {:file_info, _, :regular, _, _, _, _, _, _, _, _, _, _, _}} ->
-        path
-      {:ok, {:file_info, _, :symlink, _, _, _, _, _, _, _, _, _, _, _}} ->
-        {:ok, sym} = path |> String.to_char_list |> :file.read_link
-        case sym |> :filename.pathtype do
-          :absolute ->
-            sym |> IO.iodata_to_binary
-          :relative ->
-            symlink = sym |> IO.iodata_to_binary
-            path |> Path.dirname |> Path.join(symlink) |> Path.expand
-        end
-    end |> String.replace(~r(/bin/elixir$), "")
-  end
-
 end
