259b6b8173872adb1da9ba731e7ed4a9bb258b0b
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-08-11 17:28:11 -0500

    Update to conform 0.16.x. Fixes #184

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule ReleaseManager.Mixfile do
   def application, do: []
 
   def deps do
-    [{:conform, "~> 0.15.1"},
+    [{:conform, "~> 0.16.0"},
      {:relx, "~> 3.1.0" },
      {:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.5", only: :dev},
