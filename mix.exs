488c88bb0d98620a70a7afaf5c584e9d938b2c71
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-08-12 11:11:41 -0500

    Version 0.18.7

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ReleaseManager.Mixfile do
 
   def project do
     [ app: :exrm,
-      version: "0.18.6",
+      version: "0.18.7",
       elixir: "~> 1.0.5",
       description: description,
       package: package,
