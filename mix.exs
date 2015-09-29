9f38e3deaf6d6a9913c2c78ec794f79bcd7d6788
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-09-29 17:57:25 -0500

    Remove conform dependency

--- mix.exs
+++ mix.exs
@@ -14,8 +14,7 @@ defmodule ReleaseManager.Mixfile do
   def application, do: []
 
   def deps do
-    [{:conform, "~> 0.16.0"},
-     {:relx, "~> 3.5.0" },
+    [{:relx, "~> 3.5.0" },
      {:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.9", only: :dev},
      {:coverex, "~> 1.4.1", only: :test}]
