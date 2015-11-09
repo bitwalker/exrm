78895a301369c260a3d1765441abb7d2a1fb0c39
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-11-09 10:29:28 -0600

    Tweak logging in integration test

--- test/utils_test.exs
+++ test/utils_test.exs
@@ -70,8 +70,8 @@ defmodule UtilsTest do
     with_app do
       #capture_io(fn ->
         # Build release
-        assert :ok = Utils.mix("do deps.get, compile", Mix.env, :verbose)
-        assert :ok = Utils.mix("release --verbosity=verbose", Mix.env)
+        assert :ok = Utils.mix("do deps.get, compile", Mix.env, :quiet)
+        assert :ok = Utils.mix("release --verbosity=verbose", Mix.env, :verbose)
         assert [{"test", "0.0.1"}] == Utils.get_releases("test")
         # Boot it, ping it, and shut it down
         bin_path = Path.join([File.cwd!, "rel", "test", "bin", "test"])
