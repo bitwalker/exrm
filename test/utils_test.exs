3d1a339da521d1d99bdd65e5be615d387300e80f
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-11-09 10:26:21 -0600

    Verbose logging in integration test

--- test/utils_test.exs
+++ test/utils_test.exs
@@ -71,7 +71,7 @@ defmodule UtilsTest do
       #capture_io(fn ->
         # Build release
         assert :ok = Utils.mix("do deps.get, compile", Mix.env, :verbose)
-        assert :ok = Utils.mix("release", Mix.env)
+        assert :ok = Utils.mix("release --verbosity=verbose", Mix.env)
         assert [{"test", "0.0.1"}] == Utils.get_releases("test")
         # Boot it, ping it, and shut it down
         bin_path = Path.join([File.cwd!, "rel", "test", "bin", "test"])
