aa652122d80089d8d50f5a3c8ea4f1a6efa7610a
Paul Schoenfelder <paulschoenfelder@gmail.com>
2015-11-10 15:12:23 -0600

    Fix issues with upgrades and protocol consolidation. Closes #232

--- lib/exrm/plugins/consolidation.ex
+++ lib/exrm/plugins/consolidation.ex
@@ -7,7 +7,7 @@ defmodule ReleaseManager.Plugin.Consolidation do
   alias  ReleaseManager.Utils
   import ReleaseManager.Utils
 
-  def before_release(%Config{verbosity: verbosity, env: env}) do
+  def before_release(%Config{verbosity: verbosity, env: env} = config) do
     build_embedded = Keyword.get(Mix.Project.config, :build_embedded, false)
     should_compile = env != :test && !build_embedded
     if should_compile do
@@ -26,15 +26,22 @@ defmodule ReleaseManager.Plugin.Consolidation do
     if env != :test do
       debug "Packaging consolidated protocols..."
 
-      relx_config = Utils.rel_file_dest_path("relx.config") |> Utils.read_terms
       # Add overlay to relx.config which copies consolidated dir to release
       consolidated_path = Path.join([File.cwd!, "_build", "#{env}", "consolidated"])
-      overlays = [overlay: [
-        {:copy, '#{consolidated_path}', 'lib'}
-      ]]
-      updated = Utils.merge(relx_config, overlays)
-      # Persist relx.config
-      Utils.write_terms(Utils.rel_file_dest_path("relx.config"), updated)
+      case File.ls(consolidated_path) do
+        {:error, _} -> :ok
+        {:ok, filenames} ->
+          dest_path = "lib/#{config.name}-#{config.version}"
+          overlays = [overlay: Enum.map(filenames, fn name ->
+            {:copy, '#{consolidated_path}/#{name}', '#{Path.join(dest_path, name)}'}
+          end)]
+          updated = "relx.config"
+                    |> Utils.rel_file_dest_path
+                    |> Utils.read_terms
+                    |> Utils.merge(overlays)
+          # Persist relx.config
+          Utils.write_terms(Utils.rel_file_dest_path("relx.config"), updated)
+      end
     end
   end
 
