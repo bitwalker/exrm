f3da2750d4c5acc20136fa363b005c6e2c7f70df
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-02-15 11:42:08 -0600

    Update relx dependency

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule ReleaseManager.Mixfile do
   ]
 
   def deps do
-    [{:relx, "~> 3.5.0" },
+    [{:relx, "~> 3.5" },
      {:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.10", only: :dev},
      {:coverex, "~> 1.4", only: :test}]
